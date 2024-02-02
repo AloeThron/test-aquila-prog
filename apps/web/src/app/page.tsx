66803c40e83ee32cf0d685f0d4fb3d8962574235
AloeThron <lxe.tr.deat@gmail.com>
2024-02-03 03:54:39 +0700

    test husky

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,8 @@ import Hero from "../components/hero";
 import Contact from "./contact/page";
 
 export default function Web() {
+
+  
   return (
     <div className="container mx-auto">
       <div className="relative flex flex-col">
